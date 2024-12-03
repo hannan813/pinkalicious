c645d151dec9d95ebfedd559a09c8518dc160c84
itzabedoya <duenasitza30@gmail.com>
2024-12-03 13:12:00 -0800

    mario themed stuff

--- src/main/java/edu/sdccd/cisc190/EndOfYearProject.java
+++ src/main/java/edu/sdccd/cisc190/EndOfYearProject.java
@@ -3,6 +3,8 @@ package edu.sdccd.cisc190;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 
